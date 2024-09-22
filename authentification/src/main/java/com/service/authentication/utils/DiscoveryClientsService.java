87992f2d4bf662d6acc0855384bbcb7d7529bf00
GustavoPaludo <gustavopaludo@outlook.com>
2024-09-22 19:56:17 -0300

    feat: adding jpa and db connection configuration
    adding database configuration for the services. Added a docker-compose with the necessary database connection configurations

--- authentification/src/main/java/com/service/authentication/utils/DiscoveryClientsService.java
+++
@@ -1,8 +0,0 @@
-package com.service.authentication.utils;
-
-import org.springframework.cloud.client.ServiceInstance;
-
-public interface DiscoveryClientsService {
-
-	public ServiceInstance getServiceInstanceByServiceName(String serviceName);
-}
